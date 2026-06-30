d8f6c8c5b02d361d597420adb95f326c8331fc72
Timofei1412 <tima.fedorov.08@bk.ru>
2026-06-30 20:39:58 +0500

    fixed venv

--- сommunicate.py
+++ сommunicate.py
@@ -7,11 +7,6 @@ from typing import List, Optional, Dict, Any
 from datetime import datetime
 
 class ESPCommunication:
-    # TX: 'M'(1) + 4*int16(8) + 4*uint16(8) = 17 байт
-    TX_STRUCT = struct.Struct('<BB4b4b')
-    # RX: 'M'(1) + uint8(1) + 4*int32(16) = 18 байт
-    RX_STRUCT = struct.Struct('<Bi4i')
-    RX_HEADER = b'M'
     RX_PACKET_SIZE = 15
 
     def __init__(self, port: str = '/dev/ttyAMA0', baud: int = 115200, debug: bool = False):
@@ -44,7 +39,6 @@ class ESPCommunication:
         while self.running:
             if self.virtualConnection or not self.ser:
                 return
-
             try:
                 if self.ser.in_waiting > 7:
                     data = self.ser.read()
@@ -108,19 +102,19 @@ class ESPCommunication:
 
 
 if __name__ == "__main__":
-    # logging.basicConfig(level=logging.INFO)
-    timestamp = datetime.now().strftime('%d_%H-%M-%S')
-    log_file = f'Output/Logs/app_{timestamp}.log'
+    # # logging.basicConfig(level=logging.INFO)
+    # timestamp = datetime.now().strftime('%d_%H-%M-%S')
+    # log_file = f'Output/Logs/app_{timestamp}.log'
 
-    logging.basicConfig(
-        filename=log_file,
-        filemode="w",
-        # datefmt='%H:%M:%S.%f',
-        format='%(asctime)s | %(levelname)-8s | %(filename)s | %(message)s',
-        level=logging.INFO,
-        encoding='utf-8',
-        # backupCount=3,
-        )
+    # logging.basicConfig(
+    #     filename=log_file,
+    #     filemode="w",
+    #     # datefmt='%H:%M:%S.%f',
+    #     format='%(asctime)s | %(levelname)-8s | %(filename)s | %(message)s',
+    #     level=logging.INFO,
+    #     encoding='utf-8',
+    #     # backupCount=3,
+    #     )
     esp = ESPCommunication()
     a = time.time()
     while esp.get_last_packet() != [1]:
@@ -128,13 +122,11 @@ if __name__ == "__main__":
         print((time.time() - a)*1000)
         a = time.time()
         # time.sleep(.1)
+    
     for i in range(100):
         esp.sendMotionCommand(0, [10, 0, 0, 0], [0, 0, 0, 0])
         time.sleep(.1)
+    
     esp.sendMotionCommand(0, [0, 0, 0, 0], [0, 0, 0, 0])
     # print(esp.get_last_packet())
     esp.close()
-
-
-# 4d000000000500000000000000000000000000000000000000000000000000000000000000
-# 4d00|0005|0000|0000|0000|0000|0000|0000|0000|00
